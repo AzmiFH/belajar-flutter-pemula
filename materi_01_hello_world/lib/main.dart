f49aa6589dc9614eb9810ffe30c6e10c4ced0c01
Azmi Fajarul Haq <azmialhaq17@gmail.com>
2025-12-15 10:53:11 +0700

    widget

--- materi_01_hello_world/lib/main.dart
+++ materi_01_hello_world/lib/main.dart
@@ -1,45 +1,21 @@
 import 'package:flutter/material.dart';
+import 'StatelessWidget dan StatefulWidget/headingStatelessWidget.dart';
+import 'StatelessWidget dan StatefulWidget/StatefulWidget.dart';
+
 void main() {
- runApp(MyApp());
-}
- 
-class MyApp extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'Flutter Demo',
-     theme: ThemeData(
-       primarySwatch: Colors.blue,
-     ),
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text('Hello, world!'),
-       ),
-       body: Center(
-         child: Heading(text: "Hello World"),
-       ),
-     ),
-   );
- }
+  runApp(MyApp());
 }
 
-class Heading extends StatelessWidget {
- 
-  final String text;
- 
-  const Heading({Key? key, required this.text}) : super(key: key);
- 
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Text(
-      text,
-      style: const TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello, world!')),
+        body: Center(child: BiggerText(text: "Hello World")),
       ),
     );
   }
 }
-
-
-
