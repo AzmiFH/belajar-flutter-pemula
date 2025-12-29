92d3186d7f4a5549a3682d116596e379d6a7a42d
Azmi Fajarul Haq <azmialhaq17@gmail.com>
2025-12-29 23:02:26 +0700

    codelab 1

---
+++ codelab1_aplikasi_tempat_wisata/android/app/src/main/kotlin/com/example/codelab1_aplikasi_tempat_wisata/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.codelab1_aplikasi_tempat_wisata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ codelab1_aplikasi_tempat_wisata/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: const DetailScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Text('Farm House'),
+              margin: const EdgeInsets.only(top: 16.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
