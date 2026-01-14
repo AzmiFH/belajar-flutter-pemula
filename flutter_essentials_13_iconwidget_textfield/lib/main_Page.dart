395a9a1d8f656404c8f9f18b041bc35789aa6e56
AzmiFH <azmialhaq17@gmail.com>
2026-01-14 20:48:59 +0700

    pushhsaja

---
+++ flutter_essentials_13_iconwidget_textfield/lib/main_Page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+appBar: AppBar(
+  backgroundColor: Colors.grey.shade300,
+  title: Text("icon widget"),
+),
+body: const Center(
+  child: Icon(Icons.home),
+),
+    );
+  }
+}
