21984d2efdd0a8991dbf4a7fca9c39313fafd77e
Azmi FH <azmialhaq17@gmail.com>
2026-01-18 19:55:42 +0700

    m

---
+++ flutter_essentials_16_dropdownbutton_gesture_detector/lib/dropDownButton.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class myDropdownButton extends StatefulWidget {
+  const myDropdownButton({super.key});
+
+  @override
+  State<DropdownButton> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<DropdownButton> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
